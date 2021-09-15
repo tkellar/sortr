be89e955124dfb84fbf16e6f7aaf44a7691d0eca
tkellar <tristandkellar@gmail.com>
2021-09-14 21:32:33 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import File from './shared/components/File';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <File extension="PDF" />
+  </div>
+);
+
+export default App;
---
+++ src/shared/components/File.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Draggable from './Draggable';
+
+const FileWrapper = styled.div`
+  height: 110px;
+  width: 85px;
+  box-shadow: 0 0 10px 5px #00000066;
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  color: ${(props) => props.color};
+`;
+
+const File = ({ extension }) => {
+  const getTextColor = (ext) => {
+    switch (ext) {
+      default:
+        return 'red';
+    }
+  };
+
+  return (
+    <Draggable className="File">
+      <FileWrapper color={getTextColor(extension)}>
+        <span className="m-0 p-2 h3">{extension}</span>
+      </FileWrapper>
+    </Draggable>
+  );
+};
+
+File.propTypes = {
+  extension: PropTypes.string.isRequired,
+};
+
+export default File;
