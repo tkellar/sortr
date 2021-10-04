8119ed714d85e993561ed236e1405643a5675638
tkellar <tristandkellar@gmail.com>
2021-10-04 09:15:21 -0400

    Use ts-node-dev for development

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,22 @@
 import app from './app';
 import mongoose from 'mongoose';
-import { exit } from 'process';
 
 const conn = process.env.MONGODB_CONNECTION;
 if (conn) {
-  mongoose.connect(conn, () => {
-    console.log('Successfully connected to MongoDB instance at', conn);
-
-    app.listen(process.env.SORTR_SERVER_PORT, () => {
-      console.log(`Listening on port ${process.env.SORTR_SERVER_PORT}`);
+  mongoose
+    .connect(conn)
+    .then(() => console.log('Successfully connected to MongoDB instance at', conn))
+    .then(() =>
+      app.listen(process.env.SORTR_SERVER_PORT, () => {
+        console.log('Listening on port', process.env.SORTR_SERVER_PORT);
+      }),
+    )
+    .catch((err) => {
+      console.log('Unable to establish connection to MongoDB database at', conn);
+      console.error(err);
+      process.exit(1);
     });
-  });
 } else {
-  console.error('No MongoDB connection string provided');
-  exit(1);
+  console.log('Please provide a value for MONGODB_CONNECTION');
+  process.exit(1);
 }
